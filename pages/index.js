41cdc70bd656ac5be1702d66cef986626eac26a4
Omar Ouhra <omarmaroc26@gmail.com>
2021-08-21 13:45:24 +0100

    scroll animation

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,32 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   ArrowCircleDownIcon,
   ArrowCircleLeftIcon,
   ArrowCircleRightIcon,
   ArrowCircleUpIcon,
-  BeakerIcon,
 } from "@heroicons/react/solid";
 
 export default function Home() {
   const [isSwitched, setIsSwitched] = useState(false);
   const [isActive, setIsActive] = useState(1);
+  const [activeColor, setActiveColor] = useState(false);
+
+  const listenScrollEvent = () => {
+    const menu =
+      document.querySelector("#menu").getBoundingClientRect().bottom -30;
+
+    if (window.scrollY >= menu) {
+      setActiveColor(true)
+    } else {
+      setActiveColor(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenScrollEvent);
+  }, []);
+
   return (
     <div
       className={
@@ -52,7 +68,12 @@ export default function Home() {
         </section>
         <section
           id='menu'
-          className='bg-yellow-200 w-full h-screen flex items-center justify-center space-x-24'>
+          className=' relative bg-yellow-200 w-full h-screen flex items-center justify-center space-x-24'>
+          <a
+            href='#scrollAnimation'
+            className='text-md   absolute top-4 right-4 animate-bounce'>
+            Scroll down
+          </a>
           <div className='grid grid-cols-3 gap-1  h-7 '>
             <ArrowCircleUpIcon
               onClick={() => {
@@ -80,13 +101,22 @@ export default function Home() {
             />
           </div>
 
-          <div className='w-auto  flex items-center justify-center mt-24 '>
+          <div className='w-auto   flex items-center justify-center mt-24 '>
             <p className={isActive === 1 ? "active" : "inactive"}>Up</p>
             <p className={isActive === 2 ? "active" : "inactive"}>Left</p>
             <p className={isActive === 3 ? "active" : "inactive"}>Right</p>
             <p className={isActive === 4 ? "active" : "inactive"}>Down</p>
           </div>
         </section>
+
+        <section
+          id='scrollAnimation'
+          className={
+            activeColor
+              ? "h-screen bg-blue-500 transition-all duration-1000"
+              : "h-screen bg-black transition-all duration-1000"
+          }></section>
+      
       </main>
     </div>
   );
